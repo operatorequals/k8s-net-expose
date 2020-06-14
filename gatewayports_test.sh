503408844d3d4c1331679a8997f6d58dae9da9f8
operatorequals <john.torakis@gmail.com>
2020-06-14 17:03:50 +0300

    Test script for `GatewayPorts` SSH server option
    Introduced `gatewayports_test.sh` that checks and adds the ssh option
    needed to allow remote SSH port forwarding on all interfaces, set
    'GatewayPorts yes`.

---
+++ gatewayports_test.sh
@@ -0,0 +1,19 @@
+SSHD_CONFIG="/etc/ssh/sshd_config"
+SSHD_CONFIG="/tmp/test2"
+
+CONFIG_LINE="$(grep 'GatewayPorts' $SSHD_CONFIG)"
+CONFIG_LINE_OPTION="$(echo $CONFIG_LINE | grep 'yes' )"
+
+if [ -z "$CONFIG_LINE" ]; then
+	echo "'GatewayPorts' configuration not existing in '$SSHD_CONFIG'"
+	echo 'GatewayPorts yes' > $SSHD_CONFIG
+	echo 'Added:'
+	echo "$(tail -3 $SSHD_CONFIG)"
+elif [ -z "$CONFIG_LINE_OPTION" ]; then
+	echo "'GatewayPorts' configuration not set in '$SSHD_CONFIG'"
+	sed -i.bkp 's/GatewayPorts\s\+no/GatewayPorts yes/' $SSHD_CONFIG
+	echo 'Set:'
+	echo "$(grep -n -A1 'GatewayPorts yes' $SSHD_CONFIG)"
+else
+	echo "'GatewayPorts yes' is already set"
+fi
